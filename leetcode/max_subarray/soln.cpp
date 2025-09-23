2fe13baad9a025911a746411ad3cd9bfc9a1b4a0
xpanvictor <xpanvictor@gmail.com>
2025-09-23 21:25:17 +0100

    leetcode: max subarray solved

--- leetcode/max_subarray/soln.cpp
+++ leetcode/max_subarray/soln.cpp
@@ -4,10 +4,26 @@ using namespace std;
 
 // shorts
 typedef long long ll;
+typedef vector<int> vi;
+
+int maxSubArray(vector<int> &nums)
+{
+    int best, sum = 0, len = nums.size();
+    for (int i = 0; i < len; i++)
+    {
+        sum = max(nums[i], sum + nums[i]);
+        best = max(sum, best);
+    }
+    return best;
+}
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(NULL);
-    
+
+    vi nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int exp = 6;
+    int res = maxSubArray(nums);
+    printf("%d; true: %d\n", res, exp == res);
 }
