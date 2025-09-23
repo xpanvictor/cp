f4ee869cf823dd7c97965667fba47bbee49e4031
xpanvictor <xpanvictor@gmail.com>
2025-09-23 21:31:23 +0100

    leetcode: max subarray edge case negative single

--- leetcode/max_subarray/soln.cpp
+++ leetcode/max_subarray/soln.cpp
@@ -8,7 +8,7 @@ typedef vector<int> vi;
 
 int maxSubArray(vector<int> &nums)
 {
-    int best, sum = 0, len = nums.size();
+    int best = nums[0], sum = 0, len = nums.size();
     for (int i = 0; i < len; i++)
     {
         sum = max(nums[i], sum + nums[i]);
@@ -22,7 +22,7 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(NULL);
 
-    vi nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    vi nums = {};
     int exp = 6;
     int res = maxSubArray(nums);
     printf("%d; true: %d\n", res, exp == res);
